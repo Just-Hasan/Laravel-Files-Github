b2a4f9ca124ec522d807402b470ba298e9114e35
Just-Hasan-Desktop <109896294+Just-Hasan@users.noreply.github.com>
2024-10-13 14:48:34 +0800

    Update Laravel Files Github

--- c030323087-project-activ/app/Models/WorkshopBenefit.php
+++ c030323087-project-activ/app/Models/WorkshopBenefit.php
@@ -4,15 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Product extends Model
+class WorkshopBenefit extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
     protected $fillable = [
-        'image',
-        'title',
-        'description',
-        'price',
-        'stock'
+        "name",
+        "workshop_id",
     ];
 }
